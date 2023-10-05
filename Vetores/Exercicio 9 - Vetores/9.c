bbe946489ca05749d25996ae595af31f1ae9ab17
CauaCarloszks <135180171+CauaCarloszks@users.noreply.github.com>
2023-10-04 21:47:41 -0300

    Add files via upload

---
+++ Vetores/Exercicio 9 - Vetores/9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+main(){
+    int vetor[5], num, contador = 0;
+
+    do{
+        printf("Digite um numero par: ");
+        scanf("%d", &num);
+        if(num % 2 == 0){
+            vetor[contador] = num;
+            contador++;
+        }else{
+             printf("Numero invalido. Digite novamente: ");
+        }
+    }while(contador < 5);
+
+    for(int i = 0; i < 5; i++){
+        printf("\n%d", vetor[i]);
+    }
+
+    for(int i = 4; i >= 0; i--){
+        printf("\n%d", vetor[i]);
+    }
+
+}
